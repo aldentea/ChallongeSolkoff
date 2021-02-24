27ba688ed7bb9f4c656e5ebd3512b98002abd53c
aldentea <aldentea-dev@telepathy.aldentea.0g0.jp>
2021-02-24 17:01:12 +0900

    initial import.

---
+++ ChallongeSolkoff.Core/App.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aldentea.ChallongeSolkoff.Core
+{
+	public class App : MvvmCross.ViewModels.MvxApplication
+	{
+	}
+}
---
+++ ChallongeSolkoff/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+using MvvmCross.Core;
+
+namespace Aldentea.ChallongeSolkoff
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : MvvmCross.Platforms.Wpf.Views.MvxApplication
+	{
+		App()
+		{
+			
+			this.RegisterSetupType<MvvmCross.Platforms.Wpf.Core.MvxWpfSetup<Core.App>>();
+		}
+	}
+}
