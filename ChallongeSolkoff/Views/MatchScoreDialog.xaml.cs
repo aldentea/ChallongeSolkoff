2f9f9a0588555a5a15b790fc10f7f9981ff3bed0
aldentea <aldentea-dev@telepathy.aldentea.0g0.jp>
2021-03-19 13:49:34 +0900

    スコア入力ダイアログの外枠だけを作ってみた。

---
+++ ChallongeSolkoff/Views/MatchScoreDialog.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace Aldentea.ChallongeSolkoff.Views
+{
+	/// <summary>
+	/// MatchScoreDialog.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MatchScoreDialog : MvxWindow
+	{
+		public MatchScoreDialog()
+		{
+			InitializeComponent();
+		}
+	}
+}
